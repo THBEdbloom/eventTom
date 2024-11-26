c61d6fbad58e7f75fcbe2dc9398a296204973512
THBEdbloom <91316363+THBEdbloom@users.noreply.github.com>
2024-11-26 12:05:51 +0100

    Add files via upload

---
+++ Models/dataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eventTom.Api.Models.dataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        public DbSet<Voucher> Vouchers { get; set; }
+    }
+}
