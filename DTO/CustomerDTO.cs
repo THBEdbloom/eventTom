63d2ece3cd2b147b90606989bb6e215bdb5cbc9f
THBEdbloom <91316363+THBEdbloom@users.noreply.github.com>
2024-11-26 12:05:34 +0100

    Add files via upload

---
+++ DTO/CustomerDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace eventTom.Api.DTO
+{
+    public class CustomerDTO
+    {
+        public long CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+}
