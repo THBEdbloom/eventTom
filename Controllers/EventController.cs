0ac49023828108b9c4b64381ab7515fbbe2ef287
THBEdbloom <91316363+THBEdbloom@users.noreply.github.com>
2024-11-26 12:05:22 +0100

    Add files via upload

---
+++ Controllers/EventController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using eventTom.Api.Services.interfaces;
+
+namespace eventTom.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventsController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEvents()
+        {
+            var events = await _eventService.GetAllEventsAsync();
+            return Ok(events);
+        }
+
+        [HttpGet("{id:long}")]
+        public async Task<IActionResult> GetEventById(long id)
+        {
+            var eventDto = await _eventService.GetEventByIdAsync(id);
+            if (eventDto == null)
+                return NotFound();
+
+            return Ok(eventDto);
+        }
+    }
+}
