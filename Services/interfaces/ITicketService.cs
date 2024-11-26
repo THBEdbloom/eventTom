ae6cd376705cd1419d0e41c82a99c63e95f76eec
THBEdbloom <91316363+THBEdbloom@users.noreply.github.com>
2024-11-26 12:07:12 +0100

    Add files via upload

---
+++ Services/interfaces/ITicketService.cs
@@ -0,0 +1,12 @@
+﻿using eventTom.Api.DTO;
+
+namespace eventTom.Api.Services.interfaces
+{
+    public interface ITicketService
+    {
+        Task<TicketDTO> GetTicketByIdAsync(long id);
+        Task<IEnumerable<TicketDTO>> GetTicketsByCustomerAsync(long customerId);
+
+        Task<IEnumerable<TicketDTO>> GetTicketsByEventAsync(long eventId);
+    }
+}
